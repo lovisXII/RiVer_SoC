7bce1f4174e5e85c481d2ec41638fe5820ec82f4
LE BERRE TIMOTHEE <timothee.leberre@gmail.com>
2022-01-24 20:32:28 +0100

    Add comments, and report errors in tb

--- CORE/EXE/shifter.cpp
+++ CORE/EXE/shifter.cpp
@@ -7,6 +7,7 @@ void shifter::shifter_sll() {
     sc_uint<32> sll_dout;
     switch(shift_val)
     {
+        //from the mips code. 
         case 0:  sll_dout.range(31,0) =din.range(31,0); break;
         case 1:  sll_dout.range(31,1) =din.range(30,0); sll_dout.range( 0,0)= 0; break;
         case 2:  sll_dout.range(31,2) =din.range(29,0); sll_dout.range( 1,0)= 0; break;
@@ -48,6 +49,7 @@ void shifter::shifter_sr() {
     sc_uint<5> shift_val = SHIFT_VAL.read();
     sc_uint<32> sr_dout;
     sc_uint<32> shiftin;
+    //compute the sign extension for arithmetic shift
     if (SRA.read() & din[31]) {
         shiftin = 0xFFFFFFFF;
     }
@@ -56,38 +58,38 @@ void shifter::shifter_sr() {
     }
     switch(shift_val)
     {
-case 0:  sr_dout.range(31,0) =din.range(31,0); break;
-			case 1:  sr_dout.range(31,31)=shiftin.range(31,31); sr_dout.range(30,0)=din.range(31, 1); break;
-			case 2:  sr_dout.range(31,30)=shiftin.range(31,30); sr_dout.range(29,0)=din.range(31, 2); break;
-			case 3:  sr_dout.range(31,29)=shiftin.range(31,29); sr_dout.range(28,0)=din.range(31, 3); break;
-			case 4:  sr_dout.range(31,28)=shiftin.range(31,28); sr_dout.range(27,0)=din.range(31, 4); break;
-			case 5:  sr_dout.range(31,27)=shiftin.range(31,27); sr_dout.range(26,0)=din.range(31, 5); break;
-			case 6:  sr_dout.range(31,26)=shiftin.range(31,26); sr_dout.range(25,0)=din.range(31, 6); break;
-			case 7:  sr_dout.range(31,25)=shiftin.range(31,25); sr_dout.range(24,0)=din.range(31, 7); break;
-			case 8:  sr_dout.range(31,24)=shiftin.range(31,24); sr_dout.range(23,0)=din.range(31, 8); break;
-			case 9:  sr_dout.range(31,23)=shiftin.range(31,23); sr_dout.range(22,0)=din.range(31, 9); break;
-			case 10: sr_dout.range(31,22)=shiftin.range(31,22); sr_dout.range(21,0)=din.range(31,10); break;
-			case 11: sr_dout.range(31,21)=shiftin.range(31,21); sr_dout.range(20,0)=din.range(31,11); break;
-			case 12: sr_dout.range(31,20)=shiftin.range(31,20); sr_dout.range(19,0)=din.range(31,12); break;
-			case 13: sr_dout.range(31,19)=shiftin.range(31,19); sr_dout.range(18,0)=din.range(31,13); break;
-			case 14: sr_dout.range(31,18)=shiftin.range(31,18); sr_dout.range(17,0)=din.range(31,14); break;
-			case 15: sr_dout.range(31,17)=shiftin.range(31,17); sr_dout.range(16,0)=din.range(31,15); break;
-			case 16: sr_dout.range(31,16)=shiftin.range(31,16); sr_dout.range(15,0)=din.range(31,16); break;
-			case 17: sr_dout.range(31,15)=shiftin.range(31,15); sr_dout.range(14,0)=din.range(31,17); break;
-			case 18: sr_dout.range(31,14)=shiftin.range(31,14); sr_dout.range(13,0)=din.range(31,18); break;
-			case 19: sr_dout.range(31,13)=shiftin.range(31,13); sr_dout.range(12,0)=din.range(31,19); break;
-			case 20: sr_dout.range(31,12)=shiftin.range(31,12); sr_dout.range(11,0)=din.range(31,20); break;
-			case 21: sr_dout.range(31,11)=shiftin.range(31,11); sr_dout.range(10,0)=din.range(31,21); break;
-			case 22: sr_dout.range(31,10)=shiftin.range(31,10); sr_dout.range( 9,0)=din.range(31,22); break;
-			case 23: sr_dout.range(31, 9)=shiftin.range(31, 9); sr_dout.range( 8,0)=din.range(31,23); break;
-			case 24: sr_dout.range(31, 8)=shiftin.range(31, 8); sr_dout.range( 7,0)=din.range(31,24); break;
-			case 25: sr_dout.range(31, 7)=shiftin.range(31, 7); sr_dout.range( 6,0)=din.range(31,25); break;
-			case 26: sr_dout.range(31, 6)=shiftin.range(31, 6); sr_dout.range( 5,0)=din.range(31,26); break;
-			case 27: sr_dout.range(31, 5)=shiftin.range(31, 5); sr_dout.range( 4,0)=din.range(31,27); break;
-			case 28: sr_dout.range(31, 4)=shiftin.range(31, 4); sr_dout.range( 3,0)=din.range(31,28); break;
-			case 29: sr_dout.range(31, 3)=shiftin.range(31, 3); sr_dout.range( 2,0)=din.range(31,29); break;
-			case 30: sr_dout.range(31, 2)=shiftin.range(31, 2); sr_dout.range( 1,0)=din.range(31,30); break;
-			case 31: sr_dout.range(31, 1)=shiftin.range(31, 1); sr_dout.range( 0,0)=din.range(31,31); break;
+        case 0:  sr_dout.range(31,0) =din.range(31,0); break;
+        case 1:  sr_dout.range(31,31)=shiftin.range(31,31); sr_dout.range(30,0)=din.range(31, 1); break;
+        case 2:  sr_dout.range(31,30)=shiftin.range(31,30); sr_dout.range(29,0)=din.range(31, 2); break;
+        case 3:  sr_dout.range(31,29)=shiftin.range(31,29); sr_dout.range(28,0)=din.range(31, 3); break;
+        case 4:  sr_dout.range(31,28)=shiftin.range(31,28); sr_dout.range(27,0)=din.range(31, 4); break;
+        case 5:  sr_dout.range(31,27)=shiftin.range(31,27); sr_dout.range(26,0)=din.range(31, 5); break;
+        case 6:  sr_dout.range(31,26)=shiftin.range(31,26); sr_dout.range(25,0)=din.range(31, 6); break;
+        case 7:  sr_dout.range(31,25)=shiftin.range(31,25); sr_dout.range(24,0)=din.range(31, 7); break;
+        case 8:  sr_dout.range(31,24)=shiftin.range(31,24); sr_dout.range(23,0)=din.range(31, 8); break;
+        case 9:  sr_dout.range(31,23)=shiftin.range(31,23); sr_dout.range(22,0)=din.range(31, 9); break;
+        case 10: sr_dout.range(31,22)=shiftin.range(31,22); sr_dout.range(21,0)=din.range(31,10); break;
+        case 11: sr_dout.range(31,21)=shiftin.range(31,21); sr_dout.range(20,0)=din.range(31,11); break;
+        case 12: sr_dout.range(31,20)=shiftin.range(31,20); sr_dout.range(19,0)=din.range(31,12); break;
+        case 13: sr_dout.range(31,19)=shiftin.range(31,19); sr_dout.range(18,0)=din.range(31,13); break;
+        case 14: sr_dout.range(31,18)=shiftin.range(31,18); sr_dout.range(17,0)=din.range(31,14); break;
+        case 15: sr_dout.range(31,17)=shiftin.range(31,17); sr_dout.range(16,0)=din.range(31,15); break;
+        case 16: sr_dout.range(31,16)=shiftin.range(31,16); sr_dout.range(15,0)=din.range(31,16); break;
+        case 17: sr_dout.range(31,15)=shiftin.range(31,15); sr_dout.range(14,0)=din.range(31,17); break;
+        case 18: sr_dout.range(31,14)=shiftin.range(31,14); sr_dout.range(13,0)=din.range(31,18); break;
+        case 19: sr_dout.range(31,13)=shiftin.range(31,13); sr_dout.range(12,0)=din.range(31,19); break;
+        case 20: sr_dout.range(31,12)=shiftin.range(31,12); sr_dout.range(11,0)=din.range(31,20); break;
+        case 21: sr_dout.range(31,11)=shiftin.range(31,11); sr_dout.range(10,0)=din.range(31,21); break;
+        case 22: sr_dout.range(31,10)=shiftin.range(31,10); sr_dout.range( 9,0)=din.range(31,22); break;
+        case 23: sr_dout.range(31, 9)=shiftin.range(31, 9); sr_dout.range( 8,0)=din.range(31,23); break;
+        case 24: sr_dout.range(31, 8)=shiftin.range(31, 8); sr_dout.range( 7,0)=din.range(31,24); break;
+        case 25: sr_dout.range(31, 7)=shiftin.range(31, 7); sr_dout.range( 6,0)=din.range(31,25); break;
+        case 26: sr_dout.range(31, 6)=shiftin.range(31, 6); sr_dout.range( 5,0)=din.range(31,26); break;
+        case 27: sr_dout.range(31, 5)=shiftin.range(31, 5); sr_dout.range( 4,0)=din.range(31,27); break;
+        case 28: sr_dout.range(31, 4)=shiftin.range(31, 4); sr_dout.range( 3,0)=din.range(31,28); break;
+        case 29: sr_dout.range(31, 3)=shiftin.range(31, 3); sr_dout.range( 2,0)=din.range(31,29); break;
+        case 30: sr_dout.range(31, 2)=shiftin.range(31, 2); sr_dout.range( 1,0)=din.range(31,30); break;
+        case 31: sr_dout.range(31, 1)=shiftin.range(31, 1); sr_dout.range( 0,0)=din.range(31,31); break;
     }
     SR_DOUT.write(sr_dout);
 }
--- CORE/EXE/shifter_tb.cpp
+++ CORE/EXE/shifter_tb.cpp
@@ -1,22 +1,49 @@
 #include "shifter.h"
 #include <cstdlib>
+#include <string>
 using namespace std;
+
+//a function to print the bits of any value
+template<typename T>
+void print_bits(T ptr) {
+    size_t size = sizeof(T);
+    unsigned char *b = (unsigned char*) &ptr;
+    unsigned char byte;
+    int i, j;
+    
+    for (i = size-1; i >= 0; i--) {
+        for (j = 7; j >= 0; j--) {
+            byte = (b[i] >> j) & 1;
+            cout << (int) byte;
+        }
+    }
+}
+
 int sc_main(int argc, char* argv[])
 {
-    printf("Testing component \"Shifter\"...\n");
+    cout << "Testing component \"Shifter\"..." << endl;
+
+    shifter unit_shifter("shifter") ;
+
+    //declare signals 
     sc_signal< sc_uint<32> >  shifter_din, shifter_dout ;
     sc_signal< sc_uint<5> >  shifter_shift_val ;
     sc_signal< bool > shifter_sll, shifter_srl, shifter_sra ;
-    shifter unit_shifter("shifter") ;
+    
+    //link shifter with signals
     unit_shifter.DIN(shifter_din);
     unit_shifter.DOUT(shifter_dout);
     unit_shifter.SHIFT_VAL(shifter_shift_val);
     unit_shifter.SLL(shifter_sll);
     unit_shifter.SRL(shifter_srl);
     unit_shifter.SRA(shifter_sra);
+
+    //run one tick of simulation
     sc_start(1, SC_NS);
+
     int i;
     for (i = 0; i < 1000; i++) {
+        //compute random values for the inputs
         int sv = rand() % 16;
         int din = rand();
         int dout;
@@ -26,26 +53,54 @@ int sc_main(int argc, char* argv[])
         switch (rand() % 3) {
             case 0: 
                 sll = true; 
+                //compute the result in C
                 dout = din << sv;
                 break; 
             case 1: 
                 srl = true; 
+                //compute the result in C
                 dout = ((unsigned int) din) >> sv;
                 break; 
             case 2: 
                 sra = true; 
+                //compute the result in C
                 dout = din >> sv;
                 break; 
         }
+        //assign shifter input, and simulate
         shifter_din = din; 
         shifter_shift_val = sv;
         shifter_sll = sll;
         shifter_srl = srl;
         shifter_sra = sra;
         sc_start(1, SC_NS);
-        if (sc_uint<32>(dout) == shifter_dout) {
+        //check is result is right
+        if (sc_uint<32>(dout) != shifter_dout) {
+            cout << "Test failed !" << endl;
+            cout << "din : ";
+			print_bits(din);
+			cout << endl;
+            cout << "shift_val : ";
+			print_bits(sv);
+			cout << endl;
+            cout << "sll : ";
+			print_bits(sll);
+			cout << endl;
+            cout << "srl : ";
+			print_bits(srl);
+			cout << endl;
+            cout << "sra : ";
+			print_bits(sra);
+			cout << endl;
+            cout << "shifter : ";
+			print_bits((int) shifter_dout.read());
+			cout << endl;
+            cout << "real : ";
+			print_bits(dout);
+			cout << endl;
+            exit(0);
         }
     }
-    printf("%i random tests sucessfully passed !\n", i);
+    cout << i << " random tests sucessfully passed !" << endl;
     return 0 ;
 }
