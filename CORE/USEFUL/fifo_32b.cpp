64dc3d5fda356e115e73fa93837d4818d14971c8
lovisXII <logeofpit@yahoo.fr>
2022-01-25 20:50:17 +0100

    missing repo

--- CORE/USEFUL/fifo_32b.cpp
+++ CORE/USEFUL/fifo_32b.cpp
@@ -1,6 +1,7 @@
 #include "fifo_32b.h"
 
-void fifo_32b::function()
+void fifo_32b::processClock()
 {
     sc_signal<bool> fifo_v ;
+
 }
--- CORE/USEFUL/fifo_32b.h
+++ CORE/USEFUL/fifo_32b.h
@@ -4,14 +4,16 @@ SC_MODULE(fifo_32b)
 {
     sc_in< sc_uint<32> > din ;
     sc_in<bool> clk ;
-    sc_out< sc_uint<32> > dout ;
     sc_in<bool> push, pop ;
     sc_in<bool> reset_n ;
-    void function() ;
+    
+    sc_out< sc_uint<32> > dout ;
+ 
+    void processClock() ;
 
     SC_CTOR(fifo_32b)
     {
-        SC_METHOD(function) ;
+        SC_METHOD(processClock) ;
         sensitive << clk ;
     }
 };
