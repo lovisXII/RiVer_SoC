f1d33b0b2efde4ff73ea654b36740c246d38fff2
louis <louis@louis-VivoBook-ASUSLaptop-X509DA-M509DA>
2022-01-25 14:01:37 +0100

    fifo_32b

--- CORE/UTIL/fifo_32b.h
+++ CORE/UTIL/fifo_32b.h
@@ -2,16 +2,16 @@
 
 SC_MODULE(fifo_32b)
 {
-    sc_in< sc_uint<32> > din ;
-    sc_in<bool> clk ;
-    sc_out< sc_uint<32> > dout ;
-    sc_in<bool> push, pop ;
-    sc_in<bool> reset_n ;
+    sc_in< sc_uint<32> > DIN ;
+    sc_in_clk CLK ;
+    sc_out< sc_uint<32> > DOUT ;
+    sc_in<bool> PUSH, POP ;
+    sc_in<bool> RESET_N ;
+    
     void function() ;
 
     SC_CTOR(fifo_32b)
     {
-        SC_METHOD(function) ;
-        sensitive << clk ;
+        SC_CTHREAD(function,fifo_32b::CLK.pos()) ;
     }
 };
