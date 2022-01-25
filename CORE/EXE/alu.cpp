c9dd351f5db774dc4f22ddfc92b7a7a93f557ced
LE BERRE TIMOTHEE <timothee.leberre@gmail.com>
2022-01-25 11:43:46 +0100

    fix alu

--- CORE/EXE/alu.cpp
+++ CORE/EXE/alu.cpp
@@ -3,27 +3,23 @@
 
 void alu::operation()
     {
-        sc_uint<33> res_interne ;
-        if( cmd.read()[0] == 0 & cmd.read()[0] == 0 )
+        sc_uint<32> res_interne ;
+        if( cmd.read() == 0 )
         {
             res_interne = op1.read() + op2.read() + cin ;
-            cout.write(res_interne[32]) ;
         }
-        else if(cmd.read()[0] == 0 & cmd.read()[0] == 1 )
+        else if(cmd.read() == 1 )
         {
             res_interne = op1.read() & op2.read() ;
-            cout.write(cin.read()) ;
         }
-        else if(cmd.read()[0] == 1 & cmd.read()[0] == 0 )
+        else if(cmd.read() == 2 )
         {
             res_interne = op1.read() | op2.read() ;
-            cout.write(cin.read()) ;
         }
-        else if(cmd.read()[0] == 1 & cmd.read()[0] == 1 )
+        else if(cmd.read() == 3 )
         {
             res_interne = op1.read() ^ op2.read() ;
-            cout.write(cin.read()) ;
 
         }
-        res.write(res_interne.range(32,0)) ;
+        res.write(res_interne) ;
     }
--- CORE/EXE/alu.h
+++ CORE/EXE/alu.h
@@ -10,8 +10,7 @@ SC_MODULE(alu)
     sc_in< bool > cin ;
     sc_in< sc_uint<2> > cmd ;
     sc_out< sc_uint<32> > res ;
-    sc_out< bool > cout ;
-    
+        
     void operation() ;
 
     SC_CTOR(alu)
