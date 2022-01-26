eb572c895ec6ea9937e7ba1e8bb405268f24a68b
lovisXII <logeofpit@yahoo.fr>
2022-01-26 21:07:17 +0100

    solving test bench shifter issue

--- CORE/EXE/shifter_tb.cpp
+++ CORE/EXE/shifter_tb.cpp
@@ -29,16 +29,13 @@ int sc_main(int argc, char* argv[])
     sc_signal< sc_uint<32> >  shifter_din, shifter_dout ;
     sc_signal< sc_uint<5> >  shifter_shift_val ;
     sc_signal< sc_uint<2> > shifter_shift_cmd ;
-    //sc_signal< bool > shifter_sll, shifter_srl, shifter_sra ;
     
     //link shifter with signals
+
     unit_shifter.DIN(shifter_din);
     unit_shifter.DOUT(shifter_dout);
     unit_shifter.CMD(shifter_shift_cmd) ;
     unit_shifter.SHIFT_VAL(shifter_shift_val);
-    //unit_shifter.SLL(shifter_sll);
-    //unit_shifter.SRL(shifter_srl);
-    //unit_shifter.SRA(shifter_sra);
 
     //run one tick of simulation
     sc_start(1, SC_NS);
@@ -49,58 +46,55 @@ int sc_main(int argc, char* argv[])
         int sv = rand() % 16;
         int din = rand();
         int dout;
-        // bool sll = false;
-        // bool srl = false;
-        // bool sra = false;
         int cmd = rand() % 2 ;
-        switch (rand() % 3) {
+        
+        switch (cmd) {
             case 0: 
-                sll = true; 
                 //compute the result in C
                 dout = din << sv;
                 break; 
             case 1: 
-                srl = true; 
                 //compute the result in C
                 dout = ((unsigned int) din) >> sv;
                 break; 
             case 2: 
-                sra = true; 
                 //compute the result in C
                 dout = din >> sv;
                 break; 
         }
+        
         //assign shifter input, and simulate
+
         shifter_din = din; 
         shifter_shift_val = sv;
-        shifter_sll = sll;
-        shifter_srl = srl;
-        shifter_sra = sra;
+        shifter_shift_cmd = cmd ;
+
         sc_start(1, SC_NS);
+
         //check is result is right
+
         if (sc_uint<32>(dout) != shifter_dout) {
+            
             cout << "Test failed !" << endl;
+            
             cout << "din : ";
 			print_bits(din);
 			cout << endl;
+            
             cout << "shift_val : ";
 			print_bits(sv);
-			cout << endl;
-            cout << "sll : ";
-			print_bits(sll);
-			cout << endl;
-            cout << "srl : ";
-			print_bits(srl);
-			cout << endl;
-            cout << "sra : ";
-			print_bits(sra);
-			cout << endl;
+	        cout << endl;
+            
+            cout << "shifter command :" << cmd << endl ;
+            
             cout << "shifter : ";
 			print_bits((int) shifter_dout.read());
 			cout << endl;
+            
             cout << "real : ";
 			print_bits(dout);
 			cout << endl;
+            
             exit(0);
         }
     }
