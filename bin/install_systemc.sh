fd9e7f17f15adad09bfd7a6e95cb8d8dd04434a9
LovisXII <logeofpit@yahoo.fr>
2022-02-08 20:55:37 +0100

    testing script on linux

--- bin/install_systemc.sh
+++ bin/install_systemc.sh
@@ -12,7 +12,6 @@ else
         apt install build-essential make wget git gcc g++ automake
         wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.3.gz
         echo "Uncompressing the tar archive into current directory"
-        export CXX=g++
         tar -xzf systemc-2.3.3.gz
         
         if [ ! -d /usr/local/systemc-2.3.3/ ]
@@ -20,7 +19,6 @@ else
             mkdir /usr/local/systemc-2.3.3/
         fi
         cd systemc-2.3.3 && mkdir -p objdir && cd objdir
-        ../configure
         ../configure --prefix=/usr/local/systemc-2.3.3/
         make -j$(nproc)
         make install
