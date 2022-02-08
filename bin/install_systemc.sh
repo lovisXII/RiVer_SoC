d185b5a5556ffc6142eea62b00d92e498c27732c
lovisXII <logeofpit@yahoo.fr>
2022-02-09 00:43:36 +0100

    solving issue with script shell on wsl, needed to be execute in root

--- bin/install_systemc.sh
+++ bin/install_systemc.sh
@@ -1,14 +1,12 @@
 #This script allow you to install systemc on your system
+echo "This script need to be execute in sudo mode to properly work"
 OS="`uname`"
-
-if [ "$1" = "clean" ]
-then    
-    echo "systemc-2.3.3 and rm systemc-2.3.3.gz"
-    rm -r systemc-2.3.3 && rm systemc-2.3.3.gz
-else
+if grep -q microsoft /proc/version; then
+  echo "Ubuntu on Windows"
     if [ "$OS" = Linux ]
     then
         echo "Installing systemc for Linux distribution"
+        cd /usr/local/
         apt install build-essential make wget git gcc g++ automake
         wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.3.gz
         echo "Uncompressing the tar archive into current directory"
@@ -22,5 +20,30 @@ else
         ../configure --prefix=/usr/local/systemc-2.3.3/
         make -j$(nproc)
         make install
+    fi
+else
+  echo "native Linux"
+    if [ "$1" = "clean" ]
+    then    
+        echo "systemc-2.3.3 and rm systemc-2.3.3.gz"
+        rm -r systemc-2.3.3 && rm systemc-2.3.3.gz
+    else
+        if [ "$OS" = Linux ]
+        then
+            echo "Installing systemc for Linux distribution"
+            apt install build-essential make wget git gcc g++ automake
+            wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.3.gz
+            echo "Uncompressing the tar archive into current directory"
+            tar -xzf systemc-2.3.3.gz
+            
+            if [ ! -d /usr/local/systemc-2.3.3/ ]
+            then
+                mkdir /usr/local/systemc-2.3.3/
+            fi
+            cd systemc-2.3.3 && mkdir -p objdir && cd objdir
+            ../configure --prefix=/usr/local/systemc-2.3.3/
+            make -j$(nproc)
+            make install
+        fi
     fi
 fi
