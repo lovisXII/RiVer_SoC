741a2e864769fa363bd2daf78e07deec291555c0
kevin <kevinlastramichel@gmail.com>
2022-02-25 12:23:02 +0100

    CORE/UTIL/constant.h

--- CORE/UTIL/constant.h
+++ CORE/UTIL/constant.h
@@ -1,7 +1,8 @@
 
 
 
-// instruction type constants
+// ######## OPCODE ########
+// instruction type constants corresponding too instruction its [6, 0]
 
 const int opcode_R_type = 0b0110011;
 const int opcode_I_type = 0b0010011;
@@ -13,7 +14,10 @@ const int opcode_U_type = 0b0110111;
 const int opcode_U_type_auipc = 0b0010111;
 const int opcode_J_type = 0b1101111;
 
-// funct3
+// ######## FUNC3 ######## 
+// function3 constants coorresponding too instruction bits [14, 12]
+
+// funct3 - R-type instruction
 
 const int func3_add_i = 0b000;
 const int func3_sub_i = 0b000;
@@ -34,3 +38,65 @@ const int func3_and_i = 0b111;
 
 const int func3_lrd_i = 0b011;
 const int func3_scd_i = 0b011;
+
+
+// funct3 - I-type instruction
+
+const int funct3_add_i = 0b000;
+const int funct3_slti_i = 0b010;
+const int funct3_sltiu_i = 0b011;
+const int funct3_addi_i = 0b111;
+const int funct3_ori_i = 0b110;
+const int funct3_xori_i = 0b100;
+
+// funct3 - I-type shift instructions
+
+const int funct3_slli_i = 0b001;
+const int funct3_srli_i = 0b101;
+const int funct3_srai_i = 0b101;
+
+// funct3 - I-type load instructions
+
+const int funct3_lw_i = 0b010;
+const int funct3_lh_i = 0b001;
+const int funct3_lhu_i = 0b101;
+const int funct3_lb_i = 0b000;
+const int funct3_lbu_i = 0b100;
+
+// funct3 - S_type
+
+const int funct3_sw_i = 0b010;
+const int funct3_sh_i = 0b001;
+const int funct3_sb_i = 0b000;
+
+// funct3 - B-type
+
+const int funct3_beq_i = 0b000;
+const int funct3_bne_i = 0b001;
+const int funct3_blt_i = 0b100;
+const int funct3_bge_i = 0b101;
+const int funct3_bltu_i = 0b110;
+const int funct3_bgeu_i = 0b111;
+
+//  ######## FUNCT7 ########
+// function7 constants corresponding too instruction bits [31, 25]
+
+
+// funct7 - R-type
+
+const int funct7_add_i = 0b0000000;
+const int funct7_slt_i = 0b0000000;
+const int funct7_sltu_i = 0b0000000;
+const int funct7_and_i = 0b0000000;
+const int funct7_xor_i = 0b0000000;
+const int funct7_srl_i = 0b0000000;
+const int funct7_sub_i = 0b0100000;
+const int funct7_sra_i = 0b0100000;
+const int funct7_sll_i = 0b0000000;
+const int funct7_or_i = 0b0000000;
+
+// funct7 - I-type shift instructions
+
+const int funct7_slli_i = 0b0000000;
+const int funct7_srli_i = 0b0000000;
+const int funct7_srai_i = 0b0100000;
