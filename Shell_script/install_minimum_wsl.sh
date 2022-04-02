0f80385df984d8940415563b9243550c848f1103
LovisXII <logeofpit@yahoo.fr>
2022-04-02 16:07:13 +0200

    push script for louis wsl

---
+++ Shell_script/install_minimum_wsl.sh
@@ -0,0 +1,11 @@
+echo "Please be in sudo"
+echo "This Script setup a ws/Ubuntu envirronment"
+apt update
+apt install ghdl gtkwave
+apt install gedit
+apt install gcc
+apt install clang
+apt install ssh
+apt install git
+sh install_riscv.sh
+sh install_systemc.sh
