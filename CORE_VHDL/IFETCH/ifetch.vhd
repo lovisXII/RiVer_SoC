10c8ff218d6fb5854fbcd79aa1e94d673403d59c
Samy-Attal <samyattal@hotmail.fr>
2022-06-02 11:22:55 +0200

    [vhdl] resize if2dec fifo to 64 bits

--- CORE_VHDL/IFETCH/ifetch.vhd
+++ CORE_VHDL/IFETCH/ifetch.vhd
@@ -19,38 +19,41 @@ entity ifetch is
         DEC2IF_POP_SI : out std_logic;
 
         -- if2dec interface 
-        IF2DEC_FLUSH_SI : in std_logic;
-        IF2DEC_POP_SI : in std_logic; 
+        IF2DEC_FLUSH_SD : in std_logic;
+        IF2DEC_POP_SD : in std_logic; 
         IF2DEC_EMPTY_SI : out std_logic; 
 
-        PC_SI : in std_logic_vector(31 downto 0);
-        INSTR_SI : out std_logic_vector(31 downto 0)
+        PC_RD : in std_logic_vector(31 downto 0);
+        INSTR_RI : out std_logic_vector(31 downto 0);
+        PC_IF2DEC_RI : out std_logic_vector(31 downto 0)
     );
 end ifetch;
 
 architecture archi of ifetch is 
-signal if2dec_push, if2dec_full : std_logic;
+signal if2dec_push_si, if2dec_full_si : std_logic;
 signal stall : std_logic;
 
 begin 
 
 -- Instanciation 
 if2dec : entity work.fifo
-    generic map(N => 32)
+    generic map(N => 64)
     port map(
         clk => clk,
         reset_n => reset_n,
-        DIN => IC_INST_SI,
-        PUSH => if2dec_push, 
+        DIN(63 downto 32) => IC_INST_SI,
+        DIN(31 downto 0) => PC_RD, 
+        PUSH => if2dec_push_si, 
         POP => IF2DEC_POP_SI,
         FULL => if2dec_full, 
         EMPTY => IF2DEC_EMPTY_SI, 
-        DOUT => INSTR_SI
+        DOUT(63 downto 32) => INSTR_SI, 
+        DOUT(31 downto 0) => PC_IF2DEC_RI;
     );
 
-stall <= IC_STALL_SI or if2dec_full or DEC2IF_EMPTY_SI;
+stall <= IC_STALL_SI or if2dec_full_si or DEC2IF_EMPTY_SI;
 
-if2dec_push <= not stall when IF2DEC_FLUSH_SI = '0' else '0'; 
+if2dec_push_si <= not stall when IF2DEC_FLUSH_SI = '0' else '0'; 
 DEC2IF_POP_SI <= not stall when IF2DEC_FLUSH_SI = '0' else '1';
 ADR_VALID_SI <= not DEC2IF_EMPTY_SI when IF2DEC_FLUSH_SI = '0' else '0';
 
