1d13507752776f315e693ad4aeb9cc80763b21d1
LE BERRE TIMOTHEE <timothee.leberre@gmail.com>
2022-06-08 14:03:10 +0200

    home path

--- scripts/install_systemc.sh
+++ scripts/install_systemc.sh
@@ -3,6 +3,6 @@ apt install build-essential make wget git gcc g++ automake
 wget http://www.accellera.org/images/downloads/standards/systemc/systemc-2.3.3.gz
 tar -xzf systemc-2.3.3.gz
 cd systemc-2.3.3
-./configure --prefix=~/systemc-2.3.3/
+./configure --prefix=$(HOME)/systemc-2.3.3/
 make
 make install
