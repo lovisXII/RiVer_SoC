775fcccaa9e3f2c0d70adad24fe8853c2c5b59b9
LE BERRE TIMOTHEE <timothee.leberre@gmail.com>
2022-06-08 13:49:35 +0200

    install riscv

---
+++ scripts/install_cross_compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-2.0.0/riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz
+mkdir -p ~/riscv
+tar -xzf riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz -C ~/riscv
+
