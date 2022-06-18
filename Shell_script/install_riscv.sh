8b971b6fe8c3c5947fa311784b75343afea99dbb
LovisXII <logeofpit@yahoo.fr>
2022-06-18 11:32:12 +0200

    editing riscv shell script

--- Shell_script/install_riscv.sh
+++ Shell_script/install_riscv.sh
@@ -6,7 +6,7 @@ echo"###########################################################################
 
 
 
-if (! lsb_release -a | grep -q Scientific ) then
+if ( cat /etc/*-release | grep Ubuntu > /dev/null 2>&1) then
   wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-2.0.0/riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz
   mkdir -p /opt/riscv
   tar -xzf riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz -C /opt/riscv/
