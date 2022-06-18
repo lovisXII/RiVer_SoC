ad80d99d0f0d2665e849acf1280bdbb2f4fe705c
LovisXII <logeofpit@yahoo.fr>
2022-06-18 11:36:50 +0200

    editing riscv shell script

--- Shell_script/install_riscv.sh
+++ Shell_script/install_riscv.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo"##################################################################################################"
-echo "Don't need to sudo for Scientific Linux"
-echo "This script need to be in sudo mode otherwise you will get an error at the end of the downloading"
-echo"##################################################################################################"
 
-
+echo "This script will installed precompiled binary file for riscv cross-compiler"
+echo "Please run in sudo"
 
 if ( cat /etc/*-release | grep Ubuntu > /dev/null 2>&1) then
   wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-2.0.0/riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz
@@ -22,12 +19,5 @@ if ( cat /etc/*-release | grep Ubuntu > /dev/null 2>&1) then
     source ~/.bashrc
   fi
   rm -f riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz
-else
-  wget https://github.com/stnolting/riscv-gcc-prebuilt/releases/download/rv32i-2.0.0/riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz
-  mkdir -p ../riscv
-  tar -xzf riscv32-unknown-elf.gcc-10.2.0.rv32i.ilp32.newlib.tar.gz -C ../riscv
-  cd ..
-  echo "export PATH=$PATH:$PWD/riscv/bin" >> ~/.bashrc
-  source ~/.bashrc
 fi
 
