9bafa250eb8825fa03026cd46261da12f2a3b1fd
LE BERRE TIMOTHEE <timothee.leberre@gmail.com>
2022-06-20 15:08:21 +0200

    again

--- scripts/test_systemc_core_riscof.sh
+++ scripts/test_systemc_core_riscof.sh
@@ -2,7 +2,7 @@
 export PATH=$PATH:$HOME/riscv/bin
 export PATH=$PATH:$HOME/spike/bin
 source ~/venv/bin/activate 
-cd ../riscof
+cd riscof
 riscof validateyaml --config=config.ini
 riscof testlist --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env
 riscof run --config=config.ini --suite=riscv-arch-test/riscv-test-suite/ --env=riscv-arch-test/riscv-test-suite/env
