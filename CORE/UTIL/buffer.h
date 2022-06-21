435f0bae7b6596fc5a0338d3f4c9aa14a5226bc3
LovisXII <logeofpit@yahoo.fr>
2022-06-21 18:22:26 +0200

    starting working on buffer circular

---
+++ CORE/UTIL/buffer.h
@@ -0,0 +1,136 @@
+#pragma once
+#include <systemc.h>
+#include "debug_util.h"
+
+#define depth 4
+
+template <int T>
+
+SC_MODULE(buffer) {
+    sc_in<sc_bv<T>>     DATA_IN_S;
+    sc_in_clk           CLK;
+    sc_in<bool>         WRITE_S;
+    sc_in<bool>         READ_S;
+    sc_in<bool>         RESET_N;
+
+    sc_out<bool>        FULL_S;
+    sc_out<bool>        EMPTY_S;
+    sc_out<sc_bv<T>>    DATA_OUT_S;
+
+    sc_signal<sc_bv<32>>      inside_data_s[depth] ; 
+    sc_signal<int>              write_ptr ;
+    sc_signal<int>              read_ptr ;
+    sc_signal<bool>             full_s;
+    sc_signal<bool>             empty_s;
+    
+    void function();
+    void flags_update();
+    void trace(sc_trace_file * tf);
+    
+    SC_CTOR(buffer) 
+    {
+        SC_CTHREAD(function, buffer::CLK.pos());
+        SC_METHOD(flags_update);
+        sensitive   << RESET_N  
+                    << WRITE_S
+                    << READ_S
+                    << read_ptr 
+                    << write_ptr
+                    << DATA_IN_S;
+        for(int i = 0; i < depth ; i++){
+            sensitive << inside_data_s[i] ;
+        }
+
+        reset_signal_is(RESET_N, false);
+    }
+};
+
+template <int T>
+void buffer<T>::flags_update() {
+    if(RESET_N.read() == 0){
+        empty_s = 1 ;
+        full_s = 0;
+    }
+    else{
+        if((write_ptr == read_ptr) && WRITE_S.read()){
+            full_s = true;
+        }
+        else{
+            full_s = false;
+        }
+        if((write_ptr == read_ptr) && READ_S.read()){
+            empty_s = true;
+        }
+        else{
+            empty_s = false;
+        }
+    }
+    FULL_S = full_s ;
+    EMPTY_S = empty_s ;
+}
+
+template <int T>
+void buffer<T>::function() {
+
+    for(int i = 0 ; i < depth ; i++){
+        inside_data_s[i] = 0;
+    }
+    DATA_OUT_S  = 0;
+    write_ptr   = depth-1;
+    read_ptr    = depth-1;
+
+    wait(3);
+    while (1) {
+        bool write          = WRITE_S.read();
+        bool read           = READ_S.read();
+        int write_ptr_var   = write_ptr;
+        int read_ptr_var    = read_ptr;
+
+        if(!read && write && !full_s){
+            inside_data_s[write_ptr_var] = DATA_IN_S ;
+            write_ptr_var --;
+        }
+        else if(!write && read && !empty_s){
+            DATA_OUT_S = inside_data_s[read_ptr_var] ;
+            read_ptr_var --;
+        }
+        else if(read && write){
+            if(!empty_s)
+            {
+                DATA_OUT_S = inside_data_s[read_ptr_var] ;
+                read_ptr_var --;
+            }
+            if(!full_s){
+                inside_data_s[write_ptr_var] = DATA_IN_S ;
+                write_ptr_var --;
+            }
+        }
+        if(write_ptr_var == -1){
+            write_ptr_var = depth-1 ;
+        }
+        if(read_ptr_var == -1){
+            read_ptr_var = depth-1 ;
+        } 
+        read_ptr    = read_ptr_var ;
+        write_ptr   = write_ptr_var;
+        wait(1);
+    }
+}
+template <int T>
+void buffer<T>::trace(sc_trace_file* tf) {
+    sc_trace(tf, CLK, GET_NAME(CLK));
+    sc_trace(tf, RESET_N, GET_NAME(RESET_N));
+    sc_trace(tf, DATA_IN_S, GET_NAME(DATA_IN_S));
+    sc_trace(tf, WRITE_S, GET_NAME(WRITE_S));
+    sc_trace(tf, READ_S, GET_NAME(READ_S));
+    sc_trace(tf, FULL_S, GET_NAME(FULL_S));
+    sc_trace(tf, EMPTY_S, GET_NAME(EMPTY_S));
+    sc_trace(tf, DATA_OUT_S, GET_NAME(DATA_OUT_S));
+    sc_trace(tf, write_ptr, GET_NAME(write_ptr));
+    sc_trace(tf, read_ptr, GET_NAME(read_ptr));
+    for(int i = 0; i < depth ; i++){
+        std::string regname = "inside_data_s_";
+        regname += std::to_string(i);
+        sc_trace(tf, inside_data_s[i], signal_get_name(inside_data_s[i].name(), regname.c_str()));
+    }
+}
---
+++ CORE/UTIL/test_buffer.cpp
@@ -0,0 +1,52 @@
+#include <systemc.h>
+#include <string>
+#include "buffer.h"
+
+using namespace std;
+
+int sc_main(int argc, char* argv[]) {
+
+    sc_trace_file* tf;
+    tf = sc_create_vcd_trace_file("tf");
+
+
+    sc_signal<sc_bv<32>>    DATA_IN_S;
+    sc_clock                CLK("clk", 1, SC_NS);
+    sc_signal<bool>         WRITE_S;
+    sc_signal<bool>         READ_S;
+    sc_signal<bool>         RESET_N;
+
+    sc_signal<bool>         FULL_S;
+    sc_signal<bool>         EMPTY_S;
+    sc_signal<sc_bv<32>>    DATA_OUT_S;
+
+    buffer<32> buffer_inst("buff_test") ;
+
+    buffer_inst.DATA_IN_S(DATA_IN_S) ;
+    buffer_inst.CLK(CLK) ;
+    buffer_inst.WRITE_S(WRITE_S) ;
+    buffer_inst.READ_S(READ_S) ;
+    buffer_inst.RESET_N(RESET_N) ;
+    buffer_inst.FULL_S(FULL_S) ;
+    buffer_inst.EMPTY_S(EMPTY_S) ;
+    buffer_inst.DATA_OUT_S(DATA_OUT_S) ;
+
+
+    buffer_inst.trace(tf);
+
+
+    RESET_N.write(false);  // reset
+    sc_start(3, SC_NS);    // wait for 1 cycle
+    RESET_N.write(true);   // end of reset
+    for(int j = 0; j <1000; j++){
+        int data_in = rand() % 33 ; 
+        int write   = rand() % 2 ;
+        int read    = rand() % 2 ;
+        DATA_IN_S   = data_in ;
+        WRITE_S     = write ;
+        READ_S      = read ;
+        sc_start(1, SC_NS);
+    }
+    sc_close_vcd_trace_file(tf);
+    return 0;
+}
