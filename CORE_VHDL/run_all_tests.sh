1f89f68f2bda4d0299d2a2a19901080fa672fce3
LovisXII <logeofpit@yahoo.fr>
2022-06-27 16:12:43 +0200

    adding a automatic shell script for VHDL tests

---
+++ CORE_VHDL/run_all_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOC='\033[0m'
+make core_tb
+for file in $(ls tests); do 
+    printf "Test ${file} non opt..." 
+    timeout 20s ./core_tb tests/$file >/dev/null 2>&1
+    if (($? == 0)) 
+    then
+        printf "${GREEN} passed\n${NOC}"
+    else
+        printf "${RED} failed\n${NOC}"
+    fi
+    printf "Test ${file} opt..." 
+    timeout 20s ./core_tb tests/$file -O >/dev/null 2>&1
+    if (($? == 0)) 
+    then
+        printf "${GREEN} passed\n${NOC}"
+    else
+        printf "${RED} failed\n${NOC}"
+    fi
+
+done
