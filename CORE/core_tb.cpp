340edddb8f0ffab8ec7c139895fcdf1a958f4d80
LovisXII <logeofpit@yahoo.fr>
2022-07-04 20:58:19 +0200

    updating branch condition in core_tb to fit with ss2 architecture

--- CORE/core_tb.cpp
+++ CORE/core_tb.cpp
@@ -487,19 +487,19 @@ int sc_main(int argc, char* argv[]) {
 #endif
 
         unsigned int pc_adr = PC_VALUE.read();
-        if (signature_name == "" && pc_adr == bad_adr) {
+        if (signature_name == "" && pc_adr == (bad_adr + 4)) {
             cout << FRED("Error ! ") << "Found bad at adr 0x" << std::hex << pc_adr << endl;
             sc_start(3, SC_NS);
             exit(1);
-        } else if (signature_name == "" && pc_adr == good_adr) {
+        } else if (signature_name == "" && pc_adr == (good_adr + 4)) {
             cout << FGRN("Success ! ") << "Found good at adr 0x" << std::hex << pc_adr << endl;
             sc_start(3, SC_NS);
             exit(0);
-        } else if (signature_name == "" && pc_adr == exception_occur) {
+        } else if (signature_name == "" && pc_adr == (exception_occur + 4)) {
             cout << FYEL("Error ! ") << "Found exception_occur at adr 0x" << std::hex << pc_adr << endl;
             sc_start(3, SC_NS);
             exit(1);
-        } else if (countdown == 0 && (pc_adr == rvtest_code_end || (signature_name != "" && cycles > 20000))) {
+        } else if (countdown == 0 && (pc_adr == (rvtest_code_end +4) || (signature_name != "" && cycles > 20000))) {
             cerr << "inside if : " << endl;
             countdown = 20;
             cout << "coutndown value : " << countdown << endl;
--- CORE/tests/ss2_simple.s
+++ CORE/tests/ss2_simple.s
@@ -13,10 +13,17 @@ _start:
     beq x3,x2,_label_test_1
     addi x7,x0,120
     beq x3,x2, _label_test_2 
-
-_label_test_1 :
-    addi x5,x0,12
-
+    lw x10,0(x3)
+    addi x12,x2,3
+    slt x12,x3,x0
 
 _label_test_2 :
     ori x6,x0,12
+    lw x10,0(x3)
+    addi x12,x2,3
+    slt x12,x3,x0
+    
+_label_test_1 :
+    addi x5,x0,12
+    j _good
+    
