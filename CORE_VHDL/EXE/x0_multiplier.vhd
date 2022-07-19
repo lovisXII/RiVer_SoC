013861c5ba3ba13a9c2badb1d2c4bd7e4c1c05ae
Samy <samyattal@hotmail.fr>
2022-07-19 17:33:19 +0200

    [vhdl] add x1 and x2 multiplier, multiplier tested with basic tb

--- CORE_VHDL/EXE/x0_multiplier.vhd
+++ CORE_VHDL/EXE/x0_multiplier.vhd
@@ -6,7 +6,7 @@ library work;
 use work.util.all;
 
 entity x0_multiplier is 
-    port map(
+    port(
         -- global inteface 
         clk, reset_n    :   in  std_logic;
         OP1_SE, OP2_SE  :   in  std_logic_vector(31 downto 0);
@@ -24,20 +24,12 @@ end x0_multiplier;
 
 architecture archi of x0_multiplier is 
 
-type product_32_t is array(0 to 31) of std_logic_vector(63 downto 0);
-type product_s1_t is array(0 to 19) of std_logic_vector(63 downto 0);
-type product_s2_t is array(0 to 13) of std_logic_vector(63 downto 0);
-type product_s3_t is array(0 to 9)  of std_logic_vector(63 downto 0);
-type product_s4_t is array(0 to 5)  of std_logic_vector(63 downto 0);
-type product_s5_t is array(0 to 3)  of std_logic_vector(63 downto 0);
-
-
-signal product      : product_32_t; 
-signal product_s1   : product_s1_t;
-signal product_s2   : product_s2_t;
-signal product_s3   : product_s3_t;
-signal product_s4   : product_s4_t;
-signal product_s5   : product_s5_t;
+signal product      : tab_32_t; 
+signal product_s1   : tab_20_t;
+signal product_s2   : tab_14_t;
+signal product_s3   : tab_10_t;
+signal product_s4   : tab_6_t;
+signal product_s5   : tab_4_t;
 
 signal op1, op2     : std_logic_vector(31 downto 0);
 
@@ -91,28 +83,29 @@ x0x1 : fifo
 
 
 signed_type     <=  '1' when (MULT_CMD_RD /= "11") and ((OP1_SE(31) and OP2_SE(31)) = '0')  else '0';
-signed_res_sx0  <=  '0' when (OP1_SE(31) and OP2_SE(31) = '1') else '1'; -- else  ?? 
+signed_res_sx0  <=  '0' when (OP1_SE(31) = '1' and OP2_SE(31) = '1') else '1'; -- else  ?? 
 select_msb_sx0  <=  '1' when MULT_CMD_RD /= "01" else '0'; 
 
 op1             <=  std_logic_vector(unsigned(not(OP1_SE)) + unsigned(one_ext_32)) when (OP1_SE(31) = '1' and OP2_SE(31) = '1') else 
-                    OP2_SE when OP1_SE = '0' and OP2_SE = '1' else 
+                    OP2_SE when OP1_SE(31) = '0' and OP2_SE(31) = '1' else 
                     OP1_SE; 
 
 op2             <=  std_logic_vector(unsigned(not(OP2_SE)) + unsigned(one_ext_32)) when (OP1_SE(31) = '1' and OP2_SE(31) = '1') else 
-                    OP1_SE when OP1_SE = '0' and OP2_SE = '1' else 
+                    OP1_SE when OP1_SE(31) = '0' and OP2_SE(31) = '1' else 
                     OP2_SE; 
 
 partial_product : process(op1, op2, signed_type)
-variable prod : std_logic_vector(63 downto 0);
+variable prod : std_logic_vector(63 downto 0) := x"0000000000000000";
 begin 
     l0 : for i in 0 to 31 loop 
-        if op2[i] /= '0' then 
-            l1 : for j in 0 to i+31 loop 
-                prod := op1(j-i) and op2(i);
+        prod := x"0000000000000000";
+        if op2(i) /= '0' then 
+            l1 : for j in i to i+31 loop 
+                prod(j) := op1(j-i) and op2(i);
             end loop; 
-            if signed_type = '1' and op(31) = '1' then 
+            if signed_type = '1' and op1(31) = '1' then 
                 l2 : for k in i+31 to 63 loop
-                    prod(j) := '1'; 
+                    prod(k) := '1'; 
                 end loop; 
             end if; 
         end if; 
@@ -170,11 +163,11 @@ end generate;
 
 csa3_4 : csa 
     port map(
-        A   => product_s2(18),
-        B   => product_s2(19),
+        A   => product_s2(12),
+        B   => product_s2(13),
         C   => product(31),
-        S0  => product_s3(12),
-        S1  => product_s3(13)        
+        S0  => product_s3(8),
+        S1  => product_s3(9)        
     );
 
 stage4 : for i in 0 to 2 generate
@@ -202,8 +195,8 @@ begin
 end generate; 
 
 
-stall_sx0       <=  x0x1_full_sx0 or DEC2X0_EMPTY_SD; 
-x0x1_push_sx0   <=  not stall_sx0; 
+stall_sx0   <=  x0x1_full or DEC2X0_EMPTY_SD; 
+x0x1_push   <=  not stall_sx0; 
 
 -- fifo x0x1 input 
 x0x1_din(321)               <=  select_msb_sx0; 
@@ -214,7 +207,7 @@ x0x1_din(191 downto 128)    <=  product_s5(2);
 x0x1_din(127 downto 64)     <=  product_s5(1);
 x0x1_din(63  downto 0)      <=  product_s5(0);
 
--- fifo x0x1 input 
+-- fifo x0x1 ouput 
 SELECT_MSB_RX0              <=  x0x1_dout(321);
 SIGNED_RES_RX0              <=  x0x1_dout(320);
 RES_RX0                     <=  x0x1_dout(319 downto 0);
