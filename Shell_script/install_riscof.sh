9bb9b06baf07eb8787d9ff2686c1bf74500f0303
LovisXII <logeofpit@yahoo.fr>
2022-07-20 14:25:18 +0200

    editing scripts

--- Shell_script/install_riscof.sh
+++ Shell_script/install_riscof.sh
@@ -11,15 +11,15 @@ echo "Please run in sudo"
 echo "Installing python"
 echo "If you are running on Ubuntu 22.04 you can have some issue installing riscof, if so please run install_python_ub_22_04.sh"
 
-add-apt-repository ppa:deadsnakes/ppa
-apt-get update
-apt-get install python3.6 -y
-pip3 install --upgrade pip
+# add-apt-repository ppa:deadsnakes/ppa
+# apt-get update
+# apt-get install python3.6 -y
+# pip3 install --upgrade pip
 
 
 ################### RISCOF SETUP ################### 
 
-if [ ! [ riscof > /dev/null 2>&1 ] ]
+if ! command -v riscof &> /dev/null 
 then
     echo "Installing riscof"
     pip3 install git+https://github.com/riscv/riscof.git
@@ -31,7 +31,7 @@ fi
 echo "Installing GNU Toolchain"
 
 # sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev
-if [ ! [ riscv32-unknown-elf-gcc -v > /dev/null 2>&1 ] ] 
+if ! command -v riscv32-unknown-elf-gcc &> /dev/null 
 then 
     echo "#######################################" 
     echo "#######################################"
@@ -50,7 +50,7 @@ fi
 
 ################### SPIKE SETUP ################### 
 
-if [ ! [ spike -v > /dev/null 2>&1 ] ] 
+if ! command -v spike &> /dev/null
 then
         echo "Unlucky you, spike isn't installed : Installing spike"
         echo "#######################################" 
@@ -70,7 +70,7 @@ then
         ../configure --prefix=$TEMPORARY_PATH
         make -j4
         sudo make install #sudo is required depending on the path chosen in the previous setup
-        echo "export $PATH=/opt/spike/bin:$PATH" >> ~/.bashrc
+        echo "export PATH=/opt/spike/bin:$PATH" >> ~/.bashrc
         source ~/.bashrc
 else 
     echo "#######################################" 
@@ -88,28 +88,27 @@ fi
 ################### CONFIG SETUP ################### 
 
 cd $TEMPORARY_PATH 
-if ! [ -f "config.ini" ] 
-then
-    echo "
-    [RISCOF]
-    ReferencePlugin=spike
-    ReferencePluginPath=$PWD/RISC-V-project/riscof/spike
-    DUTPlugin=projet
-    DUTPluginPath=$PWD/RISC-V-project/riscof/projet
+rm config.ini
+echo "
+[RISCOF]
+ReferencePlugin=spike
+ReferencePluginPath=$PWD/RISC-V-project/riscof/spike
+DUTPlugin=projet
+DUTPluginPath=$PWD/RISC-V-project/riscof/projet
 
-    [spike]
-    pluginpath=$PWD/RISC-V-project/riscof/spike
-    ispec=$PWD/RISC-V-project/riscof/spike/spike_isa.yaml
-    pspec=$PWD/RISC-V-project/riscof/spike/spike_platform.yaml
-    target_run=1
+[spike]
+pluginpath=$PWD/RISC-V-project/riscof/spike
+ispec=$PWD/RISC-V-project/riscof/spike/spike_isa.yaml
+pspec=$PWD/RISC-V-project/riscof/spike/spike_platform.yaml
+target_run=1
 
-    [sail_cSim]
-    pluginpath=$PWD/RISC-V-project/riscof/sail_cSim
+[sail_cSim]
+pluginpath=$PWD/RISC-V-project/riscof/sail_cSim
 
-    [projet]
-    pluginpath=$PWD/RISC-V-project/riscof/projet
-    ispec=$PWD/RISC-V-project/riscof/projet/projet_isa.yaml
-    pspec=$PWD/RISC-V-project/riscof/projet/projet_platform.yaml
-    PATH=$PWD/RISC-V-project/CORE/core_tb">>config.ini
-fi
+[projet]
+pluginpath=$PWD/RISC-V-project/riscof/projet
+ispec=$PWD/RISC-V-project/riscof/projet/projet_isa.yaml
+pspec=$PWD/RISC-V-project/riscof/projet/projet_platform.yaml
+PATH=$PWD/RISC-V-project/CORE/core_tb">>config.ini
+
 riscof --verbose info arch-test --clone
