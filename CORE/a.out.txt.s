fdc67794fa2d8d582b6ea851284970a1752bad62
LovisXII <logeofpit@yahoo.fr>
2022-08-08 16:05:23 +0200

    new test for csr dependencies and trying to debeug riscof

--- CORE/a.out.txt.s
+++ CORE/a.out.txt.s
@@ -5,108 +5,33 @@ a.out:     file format elf32-littleriscv
 Disassembly of section seg_text:
 
 00010054 <_start>:
-   10054:	00408093          	addi	ra,ra,4
-   10058:	40110133          	sub	sp,sp,ra
-   1005c:	04c002ef          	jal	t0,100a8 <main>
-
-00010060 <modulo>:
-   10060:	fe010113          	addi	sp,sp,-32
-   10064:	00812e23          	sw	s0,28(sp)
-   10068:	02010413          	addi	s0,sp,32
-   1006c:	fea42623          	sw	a0,-20(s0)
-   10070:	feb42423          	sw	a1,-24(s0)
-   10074:	0140006f          	j	10088 <modulo+0x28>
-   10078:	fec42703          	lw	a4,-20(s0)
-   1007c:	fe842783          	lw	a5,-24(s0)
-   10080:	40f707b3          	sub	a5,a4,a5
-   10084:	fef42623          	sw	a5,-20(s0)
-   10088:	fec42703          	lw	a4,-20(s0)
-   1008c:	fe842783          	lw	a5,-24(s0)
-   10090:	fef754e3          	bge	a4,a5,10078 <modulo+0x18>
-   10094:	fec42783          	lw	a5,-20(s0)
-   10098:	00078513          	mv	a0,a5
-   1009c:	01c12403          	lw	s0,28(sp)
-   100a0:	02010113          	addi	sp,sp,32
-   100a4:	00008067          	ret
-
-000100a8 <main>:
-   100a8:	fe010113          	addi	sp,sp,-32
-   100ac:	00112e23          	sw	ra,28(sp)
-   100b0:	00812c23          	sw	s0,24(sp)
-   100b4:	02010413          	addi	s0,sp,32
-   100b8:	00300793          	li	a5,3
-   100bc:	fef42223          	sw	a5,-28(s0)
-   100c0:	00700793          	li	a5,7
-   100c4:	fef42023          	sw	a5,-32(s0)
-   100c8:	00100793          	li	a5,1
-   100cc:	fef42423          	sw	a5,-24(s0)
-   100d0:	0400006f          	j	10110 <main+0x68>
-   100d4:	fe842583          	lw	a1,-24(s0)
-   100d8:	fe442503          	lw	a0,-28(s0)
-   100dc:	f85ff0ef          	jal	ra,10060 <modulo>
-   100e0:	00050793          	mv	a5,a0
-   100e4:	02079063          	bnez	a5,10104 <main+0x5c>
-   100e8:	fe842583          	lw	a1,-24(s0)
-   100ec:	fe042503          	lw	a0,-32(s0)
-   100f0:	f71ff0ef          	jal	ra,10060 <modulo>
-   100f4:	00050793          	mv	a5,a0
-   100f8:	00079663          	bnez	a5,10104 <main+0x5c>
-   100fc:	fe842783          	lw	a5,-24(s0)
-   10100:	fef42623          	sw	a5,-20(s0)
-   10104:	fe842783          	lw	a5,-24(s0)
-   10108:	00178793          	addi	a5,a5,1
-   1010c:	fef42423          	sw	a5,-24(s0)
-   10110:	fe842703          	lw	a4,-24(s0)
-   10114:	fe442783          	lw	a5,-28(s0)
-   10118:	00e7c863          	blt	a5,a4,10128 <main+0x80>
-   1011c:	fe842703          	lw	a4,-24(s0)
-   10120:	fe042783          	lw	a5,-32(s0)
-   10124:	fae7d8e3          	bge	a5,a4,100d4 <main+0x2c>
-   10128:	fec42703          	lw	a4,-20(s0)
-   1012c:	00100793          	li	a5,1
-   10130:	00f71663          	bne	a4,a5,1013c <main+0x94>
-   10134:	ed1ef0ef          	jal	ra,4 <_good>
-   10138:	0080006f          	j	10140 <main+0x98>
-   1013c:	ec5ef0ef          	jal	ra,0 <_bad>
-   10140:	00000793          	li	a5,0
-   10144:	00078513          	mv	a0,a5
-   10148:	01c12083          	lw	ra,28(sp)
-   1014c:	01812403          	lw	s0,24(sp)
-   10150:	02010113          	addi	sp,sp,32
-   10154:	00008067          	ret
-
-Disassembly of section .comment:
-
-00000000 <.comment>:
-   0:	3a434347          	fmsub.d	ft6,ft6,ft4,ft7,rmm
-   4:	2820                	fld	fs0,80(s0)
-   6:	29554e47          	fmsub.s	ft8,fa0,fs5,ft5,rmm
-   a:	3120                	fld	fs0,96(a0)
-   c:	2e30                	fld	fa2,88(a2)
-   e:	2e32                	fld	ft8,264(sp)
-  10:	0030                	addi	a2,sp,8
+   10054:	08000537          	lui	a0,0x8000
+   10058:	00057593          	andi	a1,a0,0
+   1005c:	00059463          	bnez	a1,10064 <_start+0x10>
+   10060:	fa5ef06f          	j	4 <_good>
+   10064:	00000013          	nop
+   10068:	00000013          	nop
 
 Disassembly of section .riscv.attributes:
 
 00000000 <.riscv.attributes>:
-   0:	2441                	jal	280 <_exception_occur+0x278>
+   0:	2241                	jal	180 <_exception_occur+0x178>
    2:	0000                	unimp
    4:	7200                	flw	fs0,32(a2)
    6:	7369                	lui	t1,0xffffa
    8:	01007663          	bgeu	zero,a6,14 <_exception_occur+0xc>
-   c:	001a                	c.slli	zero,0x6
+   c:	0018                	0x18
    e:	0000                	unimp
-  10:	1004                	addi	s1,sp,32
-  12:	7205                	lui	tp,0xfffe1
-  14:	3376                	fld	ft6,376(sp)
-  16:	6932                	flw	fs2,12(sp)
-  18:	7032                	flw	ft0,44(sp)
-  1a:	5f30                	lw	a2,120(a4)
-  1c:	326d                	jal	fffff9c6 <_isr_vector+0x7efff92a>
-  1e:	3070                	fld	fa2,224(s0)
-  20:	0800                	addi	s0,sp,16
-  22:	0a01                	addi	s4,s4,0
-  24:	0b              	Address 0x0000000000000024 is out of bounds.
+  10:	7205                	lui	tp,0xfffe1
+  12:	3376                	fld	ft6,376(sp)
+  14:	6932                	flw	fs2,12(sp)
+  16:	7032                	flw	ft0,44(sp)
+  18:	5f30                	lw	a2,120(a4)
+  1a:	326d                	jal	fffff9c4 <_isr_vector+0x7efff928>
+  1c:	3070                	fld	fa2,224(s0)
+  1e:	0800                	addi	s0,sp,16
+  20:	0a01                	addi	s4,s4,0
+  22:	0b              	Address 0x0000000000000022 is out of bounds.
 
 
 Disassembly of section text:
@@ -128,72 +53,74 @@ Disassembly of section seg_reset:
 80000008:	01000e97          	auipc	t4,0x1000
 8000000c:	ff8e8e93          	addi	t4,t4,-8 # 81000000 <_exception>
 80000010:	90000f37          	lui	t5,0x90000
-80000014:	002ede93          	srli	t4,t4,0x2
-80000018:	002e9e93          	slli	t4,t4,0x2
-8000001c:	000e8e93          	mv	t4,t4
-80000020:	341e1073          	csrw	mepc,t3
-80000024:	305e9073          	csrw	mtvec,t4
-80000028:	340f1073          	csrw	mscratch,t5
-8000002c:	00010137          	lui	sp,0x10
-80000030:	01000497          	auipc	s1,0x1000
-80000034:	06c48493          	addi	s1,s1,108 # 8100009c <_isr_vector>
-80000038:	01000517          	auipc	a0,0x1000
-8000003c:	ff850513          	addi	a0,a0,-8 # 81000030 <_instruction_address_fault>
-80000040:	01000597          	auipc	a1,0x1000
-80000044:	ffc58593          	addi	a1,a1,-4 # 8100003c <_illegal_instruction>
-80000048:	01000617          	auipc	a2,0x1000
-8000004c:	fdc60613          	addi	a2,a2,-36 # 81000024 <_instruction_address_misagligned>
-80000050:	01000697          	auipc	a3,0x1000
-80000054:	02868693          	addi	a3,a3,40 # 81000078 <_env_call_u_mode>
-80000058:	01000717          	auipc	a4,0x1000
-8000005c:	02870713          	addi	a4,a4,40 # 81000080 <_env_call_s_mode>
-80000060:	01000797          	auipc	a5,0x1000
-80000064:	02878793          	addi	a5,a5,40 # 81000088 <_env_call_m_mode>
-80000068:	01000817          	auipc	a6,0x1000
-8000006c:	fe080813          	addi	a6,a6,-32 # 81000048 <_load_adress_missaligned>
-80000070:	01000897          	auipc	a7,0x1000
-80000074:	ff088893          	addi	a7,a7,-16 # 81000060 <_store_adress_missaligned>
-80000078:	01000917          	auipc	s2,0x1000
-8000007c:	fdc90913          	addi	s2,s2,-36 # 81000054 <_load_access_fault>
-80000080:	01000997          	auipc	s3,0x1000
-80000084:	fec98993          	addi	s3,s3,-20 # 8100006c <_store_access_fault>
-80000088:	01000a17          	auipc	s4,0x1000
-8000008c:	008a0a13          	addi	s4,s4,8 # 81000090 <_env_call_wrong_mode>
-80000090:	00c4a023          	sw	a2,0(s1)
-80000094:	00a4a223          	sw	a0,4(s1)
-80000098:	00b4a423          	sw	a1,8(s1)
-8000009c:	0104a823          	sw	a6,16(s1)
-800000a0:	0124aa23          	sw	s2,20(s1)
-800000a4:	0114ac23          	sw	a7,24(s1)
-800000a8:	0134ae23          	sw	s3,28(s1)
-800000ac:	02d4a023          	sw	a3,32(s1)
-800000b0:	02e4a223          	sw	a4,36(s1)
-800000b4:	02f4a423          	sw	a5,40(s1)
-800000b8:	0744a023          	sw	s4,96(s1)
-800000bc:	00000193          	li	gp,0
-800000c0:	00000213          	li	tp,0
-800000c4:	00000293          	li	t0,0
-800000c8:	00000313          	li	t1,0
-800000cc:	00000393          	li	t2,0
-800000d0:	00000413          	li	s0,0
-800000d4:	00000493          	li	s1,0
-800000d8:	00000513          	li	a0,0
-800000dc:	00000593          	li	a1,0
-800000e0:	00000613          	li	a2,0
-800000e4:	00000693          	li	a3,0
-800000e8:	00000713          	li	a4,0
-800000ec:	00000793          	li	a5,0
-800000f0:	00000813          	li	a6,0
-800000f4:	00000893          	li	a7,0
-800000f8:	00000913          	li	s2,0
-800000fc:	00000993          	li	s3,0
-80000100:	00000a13          	li	s4,0
-80000104:	00000a93          	li	s5,0
-80000108:	00000b13          	li	s6,0
-8000010c:	00000b93          	li	s7,0
-80000110:	00000c13          	li	s8,0
-80000114:	00000c93          	li	s9,0
-80000118:	30200073          	mret
+80000014:	80000db7          	lui	s11,0x80000
+80000018:	002ede93          	srli	t4,t4,0x2
+8000001c:	002e9e93          	slli	t4,t4,0x2
+80000020:	000e8e93          	mv	t4,t4
+80000024:	341e1073          	csrw	mepc,t3
+80000028:	305e9073          	csrw	mtvec,t4
+8000002c:	340f1073          	csrw	mscratch,t5
+80000030:	800d9073          	csrw	0x800,s11
+80000034:	00010137          	lui	sp,0x10
+80000038:	01000497          	auipc	s1,0x1000
+8000003c:	06448493          	addi	s1,s1,100 # 8100009c <_isr_vector>
+80000040:	01000517          	auipc	a0,0x1000
+80000044:	ff050513          	addi	a0,a0,-16 # 81000030 <_instruction_address_fault>
+80000048:	01000597          	auipc	a1,0x1000
+8000004c:	ff458593          	addi	a1,a1,-12 # 8100003c <_illegal_instruction>
+80000050:	01000617          	auipc	a2,0x1000
+80000054:	fd460613          	addi	a2,a2,-44 # 81000024 <_instruction_address_misagligned>
+80000058:	01000697          	auipc	a3,0x1000
+8000005c:	02068693          	addi	a3,a3,32 # 81000078 <_env_call_u_mode>
+80000060:	01000717          	auipc	a4,0x1000
+80000064:	02070713          	addi	a4,a4,32 # 81000080 <_env_call_s_mode>
+80000068:	01000797          	auipc	a5,0x1000
+8000006c:	02078793          	addi	a5,a5,32 # 81000088 <_env_call_m_mode>
+80000070:	01000817          	auipc	a6,0x1000
+80000074:	fd880813          	addi	a6,a6,-40 # 81000048 <_load_adress_missaligned>
+80000078:	01000897          	auipc	a7,0x1000
+8000007c:	fe888893          	addi	a7,a7,-24 # 81000060 <_store_adress_missaligned>
+80000080:	01000917          	auipc	s2,0x1000
+80000084:	fd490913          	addi	s2,s2,-44 # 81000054 <_load_access_fault>
+80000088:	01000997          	auipc	s3,0x1000
+8000008c:	fe498993          	addi	s3,s3,-28 # 8100006c <_store_access_fault>
+80000090:	01000a17          	auipc	s4,0x1000
+80000094:	000a0a13          	mv	s4,s4
+80000098:	00c4a023          	sw	a2,0(s1)
+8000009c:	00a4a223          	sw	a0,4(s1)
+800000a0:	00b4a423          	sw	a1,8(s1)
+800000a4:	0104a823          	sw	a6,16(s1)
+800000a8:	0124aa23          	sw	s2,20(s1)
+800000ac:	0114ac23          	sw	a7,24(s1)
+800000b0:	0134ae23          	sw	s3,28(s1)
+800000b4:	02d4a023          	sw	a3,32(s1)
+800000b8:	02e4a223          	sw	a4,36(s1)
+800000bc:	02f4a423          	sw	a5,40(s1)
+800000c0:	0744a023          	sw	s4,96(s1)
+800000c4:	00000193          	li	gp,0
+800000c8:	00000213          	li	tp,0
+800000cc:	00000293          	li	t0,0
+800000d0:	00000313          	li	t1,0
+800000d4:	00000393          	li	t2,0
+800000d8:	00000413          	li	s0,0
+800000dc:	00000493          	li	s1,0
+800000e0:	00000513          	li	a0,0
+800000e4:	00000593          	li	a1,0
+800000e8:	00000613          	li	a2,0
+800000ec:	00000693          	li	a3,0
+800000f0:	00000713          	li	a4,0
+800000f4:	00000793          	li	a5,0
+800000f8:	00000813          	li	a6,0
+800000fc:	00000893          	li	a7,0
+80000100:	00000913          	li	s2,0
+80000104:	00000993          	li	s3,0
+80000108:	00000a13          	li	s4,0
+8000010c:	00000a93          	li	s5,0
+80000110:	00000b13          	li	s6,0
+80000114:	00000b93          	li	s7,0
+80000118:	00000c13          	li	s8,0
+8000011c:	00000c93          	li	s9,0
+80000120:	30200073          	mret
 
 Disassembly of section seg_kernel:
 
