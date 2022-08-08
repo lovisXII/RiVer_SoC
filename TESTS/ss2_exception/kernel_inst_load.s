9357832576c249a548632daa0c987325057895be
LovisXII <logeofpit@yahoo.fr>
2022-08-08 16:05:23 +0200

    new test for csr dependencies and trying to debeug riscof

--- TESTS/ss2_exception/kernel_inst_load.s
+++ TESTS/ss2_exception/kernel_inst_load.s
@@ -7,9 +7,12 @@ _start :
     li x4,0x91231451
     sw x3,0(x4)
 <<<<<<< HEAD
+<<<<<<< HEAD
     sw x6,0(x4)
 =======
 >>>>>>> dad5b688 (new test for csr dependencies and trying to debeug riscof)
+=======
+>>>>>>> 052abf6d (new test for csr dependencies and trying to debeug riscof)
     nop
     nop
     nop
