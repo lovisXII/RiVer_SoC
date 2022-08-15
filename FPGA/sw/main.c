488d2b5614828f2516fada806b153affc9e8702e
Samy <samyattal@hotmail.fr>
2022-08-15 15:54:52 +0200

    [vhdl] add FPGA

--- FPGA/sw/main.c
+++ FPGA/sw/main.c
@@ -1,14 +1,18 @@
 __asm__(".section .init");
 __asm__(".global main");
-__asm__("j main");
 
 #include "driver_wb.h"
 #include "driver_leds.h"
 #include "driver_lcd.h"
 
 int main(void) {
-    __asm__("li x27, 0xF");
-    __asm__("li x28, 0x40000000");
-    __asm__("sw x27, 0(x28)");
+    int i = 0;
+    while(i < 10) {
+        leds_turn_on();
+        wb_wait(200);
+        leds_turn_off();
+        wb_wait(200);
+        i++;
+    }
     return 0;
 }
