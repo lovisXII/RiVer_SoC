910150548579b3cf6382b21f075bbb6fae7dc9ec
Samy <samyattal@hotmail.fr>
2022-08-22 15:17:37 +0200

    [fpga] update hw & sw

--- FPGA/hw/top.vhd
+++ FPGA/hw/top.vhd
@@ -11,13 +11,7 @@ entity top is
         LED             :   out std_logic_vector(15 downto 0);
         rgb0, rgb1      :   out std_logic_vector(2 downto 0);
         
-        -- lcd
-        lcd_rw          :   out std_logic;  
-        lcd_e           :   out std_logic;  
-        lcd_rs          :   out std_logic;  
-        lcd_data        :   out std_logic_vector(3 downto 0);
-        
-        -- ram
+        -- ram interface
         ram_adr         :   out std_logic_vector(31 downto 0);
         ram_en          :   out std_logic;
         ram_di          :   out std_logic_vector(31 downto 0);
@@ -31,14 +25,10 @@ architecture archi of top is
 constant led_base_adr   :   std_logic_vector(31 downto 0)   :=  x"40000000";
 constant led_end_adr    :   std_logic_vector(31 downto 0)   :=  x"4000000C";
 
-constant lcd_base_adr   :   std_logic_vector(31 downto 0)   :=  x"60000000";
-constant lcd_end_adr    :   std_logic_vector(31 downto 0)   :=  x"6000000C";
-
 signal clk : std_logic; 
 
 signal ack_ram  :   std_logic;
 signal ack_led  :   std_logic; 
-signal ack_lcd  :   std_logic; 
 signal ack_slv  :   std_logic; 
 
 signal stb_cpu  :   std_logic;
@@ -47,7 +37,6 @@ signal we_cpu   :   std_logic;
 signal data_cpu :   std_logic_vector(31 downto 0);
 signal data_ram :   std_logic_vector(31 downto 0);
 signal data_led :   std_logic_vector(31 downto 0);
-signal data_lcd :   std_logic_vector(31 downto 0);
 signal data_slv :   std_logic_vector(31 downto 0);
 
 signal sel_cpu  :   std_logic_vector(3 downto 0);
@@ -57,15 +46,12 @@ signal adr_cpu  :   std_logic_vector(31 downto 0);
 
 signal stb_ram  :   std_logic;
 signal stb_led  :   std_logic;
-signal stb_lcd  :   std_logic;  
 
 signal w_ram    :   std_logic;
 signal w_led    :   std_logic; 
-signal w_lcd    :   std_logic;
 
 signal select_ram   :   std_logic; 
 signal select_led   :   std_logic; 
-signal select_lcd   :   std_logic;
 
 component wb_cpu is
     port(
@@ -128,33 +114,6 @@ component wb_led
     );
 end component;
 
-component wb_lcd
-    generic(
-        BASE_ADR        :   std_logic_vector(31 downto 0);
-        DELAY           :   integer
-    );
-    port(
-        clk, reset_n    :   in  std_logic;
-
-        -- wishbone interface
-        ACK_O           :   out std_logic; 
-        STB_I           :   in  std_logic;
-        WE_I            :   in  std_logic;
-        
-        DAT_I           :   in  std_logic_vector(31 downto 0); 
-        SEL_I           :   in  std_logic_vector(3 downto 0);
-        DAT_O           :   out std_logic_vector(31 downto 0);
-        ADR_I           :   in  std_logic_vector(31 downto 0);
-        
-        -- physical interface
-        lcd_rw          :   out std_logic;  --  read / write
-        lcd_e           :   out std_logic;  --  enable
-
-        lcd_rs          :   out std_logic;  --  data / command
-        lcd_data        :   out std_logic_vector(3 downto 0)
-    );
-end component;
-
 begin
 
 clk <= clk0;  
@@ -175,7 +134,7 @@ wb_master_cpu : wb_cpu
     );
 
 wb_slave_ram : wb_ram 
-    generic map(SIZE => 4096)   -- 262144
+    generic map(SIZE => 32768)   -- 262144
     port map(
         clk             =>  clk,
         reset_n         =>  reset_n,
@@ -218,32 +177,6 @@ wb_slave_led : wb_led
         rgb0        =>  rgb0,
         rgb1        =>  rgb1        
     );
-    
-wb_slave_lcd : wb_lcd 
-    generic map(
-        BASE_ADR    =>  lcd_base_adr,
-        DELAY       =>  1000
-    )
-    port map(
-        clk         =>  clk,
-        reset_n     =>  reset_n,
-        
-        ACK_O       =>  ack_lcd,
-        STB_I       =>  stb_lcd, 
-        WE_I        =>  w_lcd, 
-        
-        DAT_I       =>  data_cpu, 
-        SEL_I       =>  sel_cpu, 
-        DAT_O       =>  data_lcd, 
-        ADR_I       =>  adr_cpu,
-        
-        lcd_rw      =>  lcd_rw,
-        lcd_e       =>  lcd_e,
-
-        lcd_rs      =>  lcd_rs,  
-        lcd_data    =>  lcd_data
-    );
-    
    
 -- adress comparator
 select_ram  <=  '1' when    adr_cpu < led_base_adr  else 
@@ -256,20 +189,12 @@ select_led  <=  '1' when    adr_cpu >= led_base_adr and adr_cpu <= led_end_adr  
 stb_led     <=  select_led and stb_cpu; 
 w_led       <=  select_led and we_cpu; 
 
-
-select_lcd  <=  '1' when    adr_cpu >=  lcd_base_adr and adr_cpu <= lcd_end_adr else 
-                '0'; 
-stb_lcd     <=  select_lcd and stb_cpu; 
-w_lcd       <=  select_lcd and we_cpu; 
-                
 data_slv    <=  data_ram    when select_ram =   '1' else 
                 data_led    when select_led =   '1' else 
-                data_lcd    when select_lcd =   '1' else
                 x"00000000";
                 
 ack_slv     <=  ack_ram     when select_ram =   '1' else 
                 ack_led     when select_led =   '1' else 
-                ack_lcd     when select_lcd =   '1' else
                 '0'; 
                                 
 end archi;
--- FPGA/sw/main.c
+++ FPGA/sw/main.c
@@ -4,15 +4,19 @@ __asm__("j main");
 
 #include "driver_wb.h"
 #include "driver_leds.h"
-#include "driver_lcd.h"
 
 int main(void) {
     int i;
     for(i=0; i<5; i++) {
         leds_turn_on();
-        wb_wait(100);
+        wb_wait(50);
         leds_turn_off();
-        wb_wait(100);
+        wb_wait(50);
     }
+    wb_write_reg(LEDS_ADR_REG1, 0x5);
+    wb_write_reg(LEDS_ADR_REG2, 0x4);
+    leds_train(20, 5);
+    river_end(); 
+        
     return 0;
 }
