42031563be41c217f634f9eeadeebd80db49f1ca
LovisXII <logeofpit@yahoo.fr>
2022-08-25 17:29:04 +0200

    test

--- scripts/build_systemc_core.sh
+++ scripts/build_systemc_core.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 printf "$(pwd)\n"
+printf "ls\n"
 ls 
+printf "ls..\n"
 ls ..
 make -C ../SIM/SystemC/CORE clean
 PATH=$PATH:$HOME/riscv/bin SYSTEMC=~/systemc-2.3.3 make -C ../SIM/SystemC/CORE 
